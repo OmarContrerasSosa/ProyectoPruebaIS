fc14193a685f273fa6063e1fc5b30bd79b064b8e
OmarContrerasSosa <omaracs12@gmail.com>
2023-04-24 13:28:34 -0600

    Impresión de mensaje como colaborador
    OACS_dev

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo en Ingeniería de Software!");
+        System.out.println("Esta es una aportación como colaborador OACS_dev");
     }
 }
